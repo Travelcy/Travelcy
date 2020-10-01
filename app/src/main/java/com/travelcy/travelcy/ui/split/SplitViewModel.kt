a670ae970e144d87c0de6d91b9cce4ba51671629
Karl Ásgeir Geirsson <karlasgeir@gmail.com>
2020-10-01 16:55:28 +0000

    Convert the fragments and tabs from the default app to Convert, Split and Settings

--- app/src/main/java/com/travelcy/travelcy/ui/split/SplitViewModel.kt
+++ app/src/main/java/com/travelcy/travelcy/ui/split/SplitViewModel.kt
@@ -1,13 +1,13 @@
-package com.travelcy.travelcy.ui.dashboard
+package com.travelcy.travelcy.ui.split
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SplitViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is the Split Fragment"
     }
     val text: LiveData<String> = _text
 }
