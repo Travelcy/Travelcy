1dcfc8ab611ceb3ac8781f07ea5be1d20a7c3414
Karl Ásgeir Geirsson <karlasgeir@gmail.com>
2020-11-05 13:34:57 +0000

    Fix initial crash when settings is not initialized and move periodic currency updates to settings repository

--- app/src/main/java/com/travelcy/travelcy/services/settings/SettingsRepository.kt
+++ app/src/main/java/com/travelcy/travelcy/services/settings/SettingsRepository.kt
@@ -1,14 +1,32 @@
 package com.travelcy.travelcy.services.settings
 
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.travelcy.travelcy.database.dao.SettingsDao
+import com.travelcy.travelcy.workers.ExchangeRateUpdateWorker
 import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 
 class SettingsRepository(
     private val settingsDao: SettingsDao,
+    private val workManager: WorkManager,
     private val executor: Executor
 ) {
     val exchangeRatesLastUpdated = settingsDao.getExchangeRatesLastUpdated()
     val autoUpdateExchangeRates = settingsDao.getAutoUpdateExchangeRates()
+
+    init {
+        autoUpdateExchangeRates.observeForever {
+            workManager.cancelAllWork()
+
+            if (it) {
+                val exchangeRateUpdateRequest = PeriodicWorkRequestBuilder<ExchangeRateUpdateWorker>(1, TimeUnit.DAYS).build()
+
+                workManager.enqueueUniquePeriodicWork(ExchangeRateUpdateWorker.WORK_NAME, ExistingPeriodicWorkPolicy.REPLACE, exchangeRateUpdateRequest);
+            }
+        }
+    }
 
     fun updateAutoUpdateExchangeRates(autoUpdate: Boolean) {
         executor.execute {
